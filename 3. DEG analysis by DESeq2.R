02f89a0ed4d6294b2ac8ffe61117bceebdc17c0a
lianggongzifang <84553701+lianggongzifang@users.noreply.github.com>
2021-05-25 20:36:02 +0800

    Update 3. DEG analysis by DESeq2.R

--- 3. DEG analysis by DESeq2.R
+++ 3. DEG analysis by DESeq2.R
@@ -8,57 +8,44 @@ library(ggrepel)
 X_data <- read.table("gene-count-matrix.txt", header = T, row.names = 1)
 names(X_data)<- c("T01", "T02", "T03", "T04", "T05", "T06", "T07", "T08", "T09", "T10")
 
-
-# 控制条件
+# 
 input_data <- X_data[c("T01", "T02", "T03", "T04", "T05", "T06")]
 condition <- factor(c(rep("WT", 3), rep("KO", 3)))
 
-# 取整
+# 
 input_data <- input_data[which(rowSums(input_data) > 0), ]
 input_data <- round(input_data, digits = 0)
-
-# 准备文件
-# as.matrix 将输入文件转换为表达矩阵；
 input_data <- as.matrix(input_data)
 
-# input_data根据控制条件构建data.frame
+# 
 coldata <- data.frame(row.names = colnames(input_data), condition)
 head(coldata)
 
-# build deseq input matrix 构建输入矩阵
-# countData作为矩阵的input_data；colData Data.frame格式；控制条件design;
+# build deseq input matrix
 dds <- DESeqDataSetFromMatrix(countData = input_data, colData = coldata, design = ~condition)
 dds$condition <- relevel(dds$condition, ref = "WT")
 
-# DESeq2 进行差异分析
+# DESeq2 
 dds <- DESeq(dds)
-# 实际包含3步
 
-# 提取结果
-# dds dataset格式转换为DESeq2 中result数据格式，矫正值默认0.1
+# 
 res <- results(dds, alpha = 0.05)
-# 查看res(DESeqresults格式),可以看到上下调基因
 summary(res)
-
-# res(resultset)按照P值排序
 res <- res[order(res$padj), ]
 res
-
 table(res$padj < 0.05)
 
-# 将进过矫正后的表达量结果加进去；
+# 
 resdata <- merge(as.data.frame(res), as.data.frame(counts(dds, normalized = T)),
                  by = "row.names", sort = F)
 names(resdata)[1] <- "ID_Gene"
-
 resdata <- separate(resdata, ID_Gene, into = c("ID", "Gene"), sep = "_")
-
 head(resdata)
 
 ######
-# 画火山图
+# Volcano Plot
 
-# 按照padj分类
+# padj
 resdata$significant <- ifelse(resdata$padj < 0.05 & resdata$log2FoldChange > 1, "UP",
                               ifelse(resdata$padj < 0.05 & resdata$log2FoldChange < -1,
                                      "DOWN", "FALSE"))
@@ -76,7 +63,7 @@ ggplot(data = resdata, aes(x = log2FoldChange, y = -log10(padj), color = signifi
   geom_text_repel(data = subset(resdata, -log10(padj) > 5 & abs(log2FoldChange) > 1), 
                   aes(label = Gene), col = "black", alpha = 0.8)
 
-# 按照pvalue分类
+# pvalue
 resdata$significant <- ifelse(resdata$pvalue < 0.05 & resdata$log2FoldChange > 1, "UP",
                               ifelse(resdata$pvalue < 0.05 & resdata$log2FoldChange < -1,
                                      "DOWN", "FALSE"))
@@ -94,7 +81,7 @@ ggplot(data = resdata, aes(x = log2FoldChange, y = -log10(pvalue), color = signi
   geom_text_repel(data = subset(resdata, -log10(pvalue) > 10 & abs(log2FoldChange) > 1), 
                   aes(label = Gene), col = "black", alpha = 0.8)
 
-# output result 输出结果
+# output results
 write.table(resdata, file = "X.KO vs WT.diffexpr-results.txt", 
             sep = "\t", quote = F, row.names = F)
 
