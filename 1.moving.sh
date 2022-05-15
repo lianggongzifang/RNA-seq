0cacecc0b189c1ec5dde349dbc4ec736d0f0d2f2
lianggongzifang <84553701+lianggongzifang@users.noreply.github.com>
2022-05-15 15:01:34 +0800

    Create 1.moving.sh

---
+++ 1.moving.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SEQUENCING_RUN="HU000"
+I7_INDEX="B41 B42 B43 B44 B45 B46 B47 B48"
+
+mkdir ${SEQUENCING_RUN}
+cd ${SEQUENCING_RUN}
+for name in ${I7_INDEX}
+do
+cp /home/storage/${SEQUENCING_RUN}/I7-${name}_R1.fq.gz ./I7-${name}_1.fq.gz
+cp /home/storage/${SEQUENCING_RUN}/I7-${name}_R2.fq.gz ./I7-${name}_2.fq.gz
+done
+
+echo "\n*** moving.sh FINISH! ***\n"
