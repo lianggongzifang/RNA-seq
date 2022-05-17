b9368c3b22771629475691e9fa04c3b8c8ccebb3
lianggongzifang <84553701+lianggongzifang@users.noreply.github.com>
2022-05-17 19:52:58 +0800

    Update 2.SMART-multx-STAR-rsem-count.sh

--- 2.SMART-multx-STAR-rsem-count.sh
+++ 2.SMART-multx-STAR-rsem-count.sh
@@ -5,7 +5,6 @@ I7_INDEX="B41 B42 B43 B44 B45 B46 B47 B48"
 SAMPLE="YF000a"
 SAMPLE_ALL="YF000"
 
-gunzip *
 mkdir -p SMART/multx SMART/cut-index SMART/STAR SMART/rsem SMART/rseqc
 cd SMART
 
@@ -14,7 +13,7 @@ cd multx
 for name in ${I7_INDEX}
 do
 fastq-multx -m 0 -d 2 -x -b -B ~/${SEQUENCING_RUN}/${SEQUENCING_RUN}-${name}_index \
-~/${SEQUENCING_RUN}/I7-${name}_1.fq ~/${SEQUENCING_RUN}/I7-${name}_2.fq -o %_R1.fq %_R2.fq  \
+~/${SEQUENCING_RUN}/I7-${name}_1.fq.gz ~/${SEQUENCING_RUN}/I7-${name}_2.fq.gz -o %_R1.fq %_R2.fq  \
 > ${SEQUENCING_RUN}-${name}_multx.txt
 done
 cd ..
