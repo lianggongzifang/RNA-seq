fa4f52888c305c6d7a5916214ddf455ff86de116
lianggongzifang <84553701+lianggongzifang@users.noreply.github.com>
2022-05-20 22:49:56 +0800

    Update 2.SMART-zUMIs.sh

--- 2.SMART-zUMIs.sh
+++ 2.SMART-zUMIs.sh
@@ -4,26 +4,17 @@ SEQUENCING_RUN="HU000"
 I7_INDEX="B41 B42 B43 B44 B45 B46 B47 B48"
 SAMPLE="YF000a"
 SAMPLE_ALL="YF000"
+PREFIX="ZUMIS"
 
-mkdir -p ZUMIS/multx ZUMIS/merge
-cd ZUMIS
-
-## multx
-cd multx
-for name in ${I7_INDEX}
-do
-fastq-multx -m 0 -d 2 -x -b -B ~/${SEQUENCING_RUN}/${SEQUENCING_RUN}-${name}_index \
-~/${SEQUENCING_RUN}/I7-${name}_1.fq.gz ~/${SEQUENCING_RUN}/I7-${name}_2.fq.gz -o %_R1.fq.gz %_R2.fq.gz \
-> ${SEQUENCING_RUN}-${name}_multx.txt
-done
-cd ..
+mkdir -p ${PREFIX}/merge
+cd ${PREFIX}
 
 ## merge reads
 cd merge
 for name in ${SAMPLE}
 do
-seqkit replace -p "\s.+" ~/${SEQUENCING_RUN}/ZUMIS/multx/${name}_R1.fq.gz > ./${name}_R1_001.fq
-seqkit replace -p "\s.+" ~/${SEQUENCING_RUN}/ZUMIS/multx/${name}_R2.fq.gz > ./${name}_R2_002.fq
+seqkit replace -p "\s.+" ~/${SEQUENCING_RUN}/multx/${name}_R1.fq.gz > ./${name}_R1_001.fq
+seqkit replace -p "\s.+" ~/${SEQUENCING_RUN}/multx/${name}_R2.fq.gz > ./${name}_R2_002.fq
 done
 gzip *
 Rscript ~/software/zUMIs/misc/merge_demultiplexed_fastq.R -d . -t 8
@@ -37,5 +28,5 @@ bash ~/software/zUMIs/zUMIs.sh -d ~/software/zUMIs/ -y ~/${SEQUENCING_RUN}/${SAM
 rm -r *._STARgenome *._STARpass1
 multiqc .
 
-rm ./multx/*.fq.gz ./merge/*.fq.gz
+rm ./merge/*.fq.gz
 echo "\n*** SMART-zUMIs.sh FINISH! ***\n"
